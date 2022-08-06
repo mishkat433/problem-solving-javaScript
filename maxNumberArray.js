df0246347a4a0042e8f76ad014f7f2c7008a8816
miskat433 <miskat433@gmail.com>
2022-08-06 22:18:36 +0600

    max number from array

---
+++ maxNumberArray.js
@@ -0,0 +1,13 @@
+function maxnumber_from_Array(array) {
+    let initial = 0;
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        if (initial < array[i]) {
+            initial = array[i]
+        }
+    }
+    return initial
+}
+const arr = [4, 5, 8, 24, 29, 25, -58, 250, 59]
+const result = maxnumber_from_Array(arr)
+console.log(result);
